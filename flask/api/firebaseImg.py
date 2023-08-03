b076e06bb4766a51fa16a7dec10923b65b14ad22
high <highcloud100@inha.edu>
2023-08-02 23:13:20 -0700

    revoke

--- flask/api/firebaseImg.py
+++ flask/api/firebaseImg.py
@@ -1,26 +1,16 @@
 import firebase_admin
 from firebase_admin import credentials, storage
 
-def firebaseDownload(filename='profile.jpg',bucket_id='bluegate-9ed94.appspot.com'): #path: 저장경로 #filename: 버켓에 있는 이미지 경로 #bucket_id: 사용할 버켓
+
+cred = credentials.Certificate("./static/serviceAccountKey.json")
+firebase_admin.initialize_app(cred, {"storageBucket":'bluegate-9ed94.appspot.com'})
+    
+
+def firebaseDownload(filename='profile.jpeg'): #path: 저장경로 #filename: 버켓에 있는 이미지 경로 #bucket_id: 사용할 버켓
     #Private key 
 
-    cred = credentials.Certificate("./static/serviceAccountKey.json")
-    firebase_admin.initialize_app(cred, {"storageBucket":bucket_id})
-    
     bucket = storage.bucket()
     print(bucket)
     
-    try:
-        #버켓에 있는 이미지 불러옴
-        blob = bucket.blob(filename)
-        print(blob)
-        #파일 저장
-        blob.download_to_filename(f'./registedIMG/{filename}')
-        
-    #에러처리    
-    except Exception as e:
-        return str(e)
-    
-firebaseDownload()
-    
-        
+    bucket
+
